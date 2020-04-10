946544ded87cb351da8c83ac36c0341b30390b79
andreiz <mcjanz@gmail.com>
2020-04-10 20:25:56 +0300

    tests and black formatting

--- analyze_delicious.py
+++ analyze_delicious.py
@@ -9,30 +9,34 @@ stat = {"dates": {}, "privates": 0, "publics": 0, "tags": {}}
 
 env = Environment(loader=FileSystemLoader("templates"))
 
+
 class Url:
     def __init__(self, url):
         self.url = url
-    
+
     def get_domain(self) -> str:
         return urlparse(self.url).netloc
+
 
 class LinkInfo:
     def __init__(self, info):
         self.info = info
-        self.url = Url(self.info['href'])
-    
+        self.url = Url(self.info["href"])
+
     @property
     def is_private(self) -> bool:
-        return bool(int(self.info['private']))
-    
+        return bool(int(self.info["private"]))
+
     @property
     def href(self) -> str:
         return self.info["href"]
-    
+
     @property
     def text(self) -> str:
-        return self.info['text']
-    
+        if self.info["text"] in ["", "None"]:
+            return self.url.get_domain()
+        return self.info["text"]
+
     def __str__(self) -> str:
         if not self.is_private:
             return f"LinkInfo: {self.url.get_domain()}"
@@ -40,47 +44,50 @@ class LinkInfo:
             return "LinkInfo is private."
 
     def get_tags(self) -> list:
-        return self.info['tags'].split(",")
+        return self.info["tags"].split(",")
 
 
 def get_links(filename):
     with open(filename) as f:
-        soup = BeautifulSoup(f.read(), 'html.parser')
-        for link in soup.find_all('a'):
+        soup = BeautifulSoup(f.read(), "html.parser")
+        for link in soup.find_all("a"):
             yield LinkInfo({**link.attrs, **{"text": link.text}})
 
-def process_stats(link_attrs, statistic):
-    if link_attrs['href'] in statistic.keys():
-        statistic[link_attrs['href']] += 1
-    else:
-        statistic[link_attrs['href']] = 1
-    
-    if link_attrs["add_date"] not in statistic["dates"].keys():
-        statistic["dates"][link_attrs['add_date']] = 1
-    else:
-        statistic["dates"][link_attrs['add_date']] += 1
 
-    if link_attrs['private']:
+def process_stats(link_attrs, statistic):
+    if link_attrs["href"] in statistic.keys():
+        statistic[link_attrs["href"]] += 1
+    else:
+        statistic[link_attrs["href"]] = 1
+
+    if link_attrs["add_date"] not in statistic["dates"].keys():
+        statistic["dates"][link_attrs["add_date"]] = 1
+    else:
+        statistic["dates"][link_attrs["add_date"]] += 1
+
+    if link_attrs["private"]:
         statistic["privates"] += 1
     else:
         statistic["publics"] += 1
-    
+
     tags = link_attrs["tags"].split(",")
     for tag in tags:
-        if tag in statistic['tags'].keys():
+        if tag in statistic["tags"].keys():
             statistic["tags"][tag] += 1
         else:
-            statistic['tags'][tag] = 1
-    
+            statistic["tags"][tag] = 1
+
     # yield link_attrs
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze your Delicious bookmarks.")
     parser.add_argument("-f", action="store", dest="filename", help="Path to file with bookmarks.")
+    parser.add_argument("-o", action="store", dest="output_file", help="Path to output file", default="report.html")
 
     results = parser.parse_args()
 
-    template = env.get_template('template.html')
-    
-    with open("report.html", "wt") as f:
+    template = env.get_template("template.html")
+
+    with open(results.output_file, "wt") as f:
         f.write(template.render(links=(l for l in get_links(results.filename))))
--- test_analyze_delicious.py
+++ test_analyze_delicious.py
@@ -4,21 +4,90 @@ from analyze_delicious import Url, LinkInfo
 class TestLink:
     def test_url_creation(self):
         l = Url("https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep")
-        assert l.url == "https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep"
+        assert (
+            l.url
+            == "https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep"
+        )
 
     def test_get_domain_for_url(self):
         assert Url("https://egghead.io/courses/getting-started-with-redux").get_domain() == "egghead.io"
 
+
 class TestLinkInfo:
     def test_link_info_get_tags(self):
-        assert LinkInfo({'href': 'http://www.smartvideos.ru/', 'add_date': '2012', 'private': '1', 'tags': 'education,imported'}).get_tags() == ["education", "imported"]
+        assert LinkInfo(
+            {"href": "http://www.smartvideos.ru/", "add_date": "2012", "private": "1", "tags": "education,imported"}
+        ).get_tags() == ["education", "imported"]
 
     def test_is_private(self):
-        assert LinkInfo({'href': 'http://www.smartvideos.ru/', 'add_date': '2012', 'private': '1', 'tags': 'education,imported'}).is_private is True
-        assert LinkInfo({'href': 'http://www.smartvideos.ru/', 'add_date': '2012', 'private': '0', 'tags': 'education,imported'}).is_private is False
-    
+        assert (
+            LinkInfo(
+                {"href": "http://www.smartvideos.ru/", "add_date": "2012", "private": "1", "tags": "education,imported"}
+            ).is_private
+            is True
+        )
+        assert (
+            LinkInfo(
+                {"href": "http://www.smartvideos.ru/", "add_date": "2012", "private": "0", "tags": "education,imported"}
+            ).is_private
+            is False
+        )
+
     def test_href(self):
-        assert LinkInfo({'href': 'http://www.smartvideos.ru/', 'add_date': '2012', 'private': '1', 'tags': 'education,imported'}).href == 'http://www.smartvideos.ru/'
+        assert (
+            LinkInfo(
+                {"href": "http://www.smartvideos.ru/", "add_date": "2012", "private": "1", "tags": "education,imported"}
+            ).href
+            == "http://www.smartvideos.ru/"
+        )
 
     def test_text(self):
-        assert LinkInfo({'href': 'http://www.smartvideos.ru/', 'add_date': '2012', 'private': '1', 'tags': 'education,imported', "text": "text2"}).text == 'text2'
+        assert (
+            LinkInfo(
+                {
+                    "href": "http://www.smartvideos.ru/",
+                    "add_date": "2012",
+                    "private": "1",
+                    "tags": "education,imported",
+                    "text": "text2",
+                }
+            ).text
+            == "text2"
+        )
+        assert (
+            LinkInfo(
+                {
+                    "href": "http://www.smartvideos.ru/",
+                    "add_date": "2012",
+                    "private": "1",
+                    "tags": "education,imported",
+                    "text": "",
+                }
+            ).text
+            == "www.smartvideos.ru"
+        )
+        assert (
+            LinkInfo(
+                {
+                    "href": "https://egghead.io/courses/getting-started-with-redux",
+                    "add_date": "2012",
+                    "private": "1",
+                    "tags": "education,imported",
+                    "text": "",
+                }
+            ).text
+            == "egghead.io"
+        )
+        assert (
+            LinkInfo(
+                {
+                    "href": "https://egghead.io/courses/getting-started-with-redux",
+                    "add_date": "2012",
+                    "private": "1",
+                    "tags": "education,imported",
+                    "text": None,
+                }
+            ).text
+            == "egghead.io"
+        )
+
