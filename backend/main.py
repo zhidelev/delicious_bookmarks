359acbe0fd4474ade59677fb5083e14779c4dd67
andreiz <mcjanz@gmail.com>
2023-08-23 23:21:24 +0200

    Fix

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Path
+from pydantic import BaseModel, HttpUrl
 from typing import List, Union
+from tag import Tags
+import json
 
 
-class Bookmark(BaseModel):
-    uri: str
+class BookmarkId(BaseModel):
+    id: int
+
+
+class BookmarkIn(BaseModel):
+    uri: HttpUrl
     description: Union[str, None] = None
+    tags: Union[List[str], None] = None
+
+
+class BookmarkOut(BookmarkIn):
+    id: int
 
 
 app = FastAPI()
+
+bookmarks = []
 
 
 @app.get("/")
@@ -16,11 +29,18 @@ def get_root():
     return {"Hello, world!"}
 
 
-@app.get("/bookmarks/{b_id}")
-def get_bookmark(b_id: int):
-    return {"bookmark": b_id}
+@app.get("/bookmarks/{b_id}", tags=[Tags.bookmarks])
+def get_bookmark(b_id: int = Path(title="The ID of the bookmark to get.", ge=1)):
+    return {"id": b_id, "bookmark": dict()}
 
 
-@app.post("/bookmarks/")
-def create_bookmark(bookmark: Bookmark):
-    return bookmark
+@app.get("/bookmarks/", tags=[Tags.bookmarks])
+def get_all_bookmarks() -> Union[List[BookmarkOut], List[None]]:
+    if bookmarks:
+        return [bookmark for bookmark in bookmarks]
+    return []
+
+
+@app.post("/bookmarks/", tags=[Tags.bookmarks])
+def create_bookmark(bookmark: BookmarkIn) -> BookmarkId:
+    return {"id": 1}
