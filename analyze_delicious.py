2e0a26f557c0bde8ed139a30097fca6b6be50aba
andreiz <mcjanz@gmail.com>
2023-08-23 23:34:23 +0200

    Dependencies

--- analyze_delicious.py
+++ analyze_delicious.py
@@ -1,227 +1,222 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-import argparse
-import logging
-import logging.config
-import pathlib
-from collections import defaultdict
-from datetime import datetime
-from urllib.parse import urlparse
-from typing import List
-from dataclasses import dataclass
-import csv
+# import argparse
+# import logging
+# import logging.config
+# import pathlib
+# from collections import defaultdict
+# from datetime import datetime
+# from urllib.parse import urlparse
+# from typing import List
+# from dataclasses import dataclass
+# import csv
 
-from bs4 import BeautifulSoup
-from jinja2 import Environment, FileSystemLoader
+# # from bs4 import BeautifulSoup
+# # from jinja2 import Environment, FileSystemLoader
 
-logging.config.fileConfig("logging.conf")
-logger = logging.getLogger("root")
-env = Environment(loader=FileSystemLoader("templates"), autoescape=True)
+# logging.config.fileConfig("logging.conf")
+# logger = logging.getLogger("root")
+# # env = Environment(loader=FileSystemLoader("templates"), autoescape=True)
 
 
-@dataclass
-class Url:
-    """Wrapper for URL"""
+# @dataclass
+# class Url:
+#     """Wrapper for URL"""
 
-    addr: str
+#     addr: str
 
-    def get_domain(self) -> str:
-        """Returns a domain for the URL
-        
-        :returns: a string with domain
-        :rtype: str
+#     def get_domain(self) -> str:
+#         """Returns a domain for the URL
 
-        """
+#         :returns: a string with domain
+#         :rtype: str
 
-        return urlparse(self.addr).netloc
+#         """
 
-    def __str__(self):
-        return self.addr
+#         return urlparse(self.addr).netloc
 
+#     def __str__(self):
+#         return self.addr
 
-@dataclass
-class LinkInfo:
-    """Represents information for a link extracted from export file"""
 
-    info: dict
+# @dataclass
+# class LinkInfo:
+#     """Represents information for a link extracted from export file"""
 
-    @property
-    def is_private(self) -> bool:
+#     info: dict
 
-        """Checks if link in LinkInfo is marked as private.
+#     @property
+#     def is_private(self) -> bool:
+#         """Checks if link in LinkInfo is marked as private.
 
-        :returns: True if private or False overwise
-        :rtype: bool
+#         :returns: True if private or False overwise
+#         :rtype: bool
 
-        """
+#         """
 
-        return True if self.info["private"] == "1" else False
+#         return True if self.info["private"] == "1" else False
 
-    @property
-    def href(self) -> str:
+#     @property
+#     def href(self) -> str:
+#         """Returns 'href' attribute
 
-        """Returns 'href' attribute
+#         :returns: link from href
+#         :rtype: str
 
-        :returns: link from href
-        :rtype: str
+#         """
 
-        """
+#         return self.info["href"]
 
-        return self.info["href"]
+#     @property
+#     def text(self) -> str:
+#         """Returns text description for a link
 
-    @property
-    def text(self) -> str:
+#         Returns text in 'text' attribute if set. If 'text' is empty of None returns a domain.
 
-        """Returns text description for a link
+#         :returns: Text description or domain
+#         :rtype: str
 
-        Returns text in 'text' attribute if set. If 'text' is empty of None returns a domain.
+#         """
 
-        :returns: Text description or domain
-        :rtype: str
+#         if self.info["text"] in ["", "None"]:
+#             return Url(self.info["href"]).get_domain()
+#         return self.info["text"]
 
-        """
+#     @property
+#     def date(self) -> str:
+#         """Returns link adding date in human-readable view
 
-        if self.info["text"] in ["", "None"]:
-            return Url(self.info["href"]).get_domain()
-        return self.info["text"]
+#         :returns: string with date
+#         :rtype: str
 
-    @property
-    def date(self) -> str:
+#         """
 
-        """Returns link adding date in human-readable view
+#         if len(self.info["add_date"]) == 4:
+#             return self.info["add_date"]
+#         t_date = datetime.fromtimestamp(int(self.info["add_date"]))
+#         return "{:%Y-%m-%d}".format(t_date)
 
-        :returns: string with date
-        :rtype: str
+#     @property
+#     def timestamp(self) -> int:
+#         """Returns timestamp of adding date
 
-        """
+#         :returns: date timestamp or 0 if date is invalid
+#         :rtype: int
 
-        if len(self.info["add_date"]) == 4:
-            return self.info["add_date"]
-        t_date = datetime.fromtimestamp(int(self.info["add_date"]))
-        return "{:%Y-%m-%d}".format(t_date)
+#         """
 
-    @property
-    def timestamp(self) -> int:
+#         if len(self.info["add_date"]) == 4:
+#             return 0
+#         else:
+#             return int(self.info["add_date"])
 
-        """Returns timestamp of adding date
+#     def __str__(self) -> str:
+#         if not self.is_private:
+#             return f"LinkInfo: {Url(self.info['href']).get_domain()}"
+#         else:
+#             return "LinkInfo is private."
 
-        :returns: date timestamp or 0 if date is invalid
-        :rtype: int
+#     def get_tags(self) -> List[str]:
+#         """Gets a list of tags from 'tags' attribute
 
-        """
+#         :returns: list with tags or empty list
+#         :rtype: list
 
-        if len(self.info["add_date"]) == 4:
-            return 0
-        else:
-            return int(self.info["add_date"])
+#         """
 
-    def __str__(self) -> str:
-        if not self.is_private:
-            return f"LinkInfo: {Url(self.info['href']).get_domain()}"
-        else:
-            return "LinkInfo is private."
+#         return self.info["tags"].split(",")
 
-    def get_tags(self) -> List[str]:
-        """Gets a list of tags from 'tags' attribute
+#     def get_domain(self):
+#         """Gets a  domain for a 'href' attribute
 
-        :returns: list with tags or empty list
-        :rtype: list
+#         :returns: domain address
+#         :rtype: str
 
-        """
+#         """
 
-        return self.info["tags"].split(",")
+#         return Url(self.info["href"]).get_domain()
 
-    def get_domain(self):
-        """Gets a  domain for a 'href' attribute
 
-        :returns: domain address
-        :rtype: str
-        
-        """
+# class Stats:
+#     def __init__(self):
+#         self.privacy = defaultdict(int)
+#         self.tags = defaultdict(int)
 
-        return Url(self.info["href"]).get_domain()
+#     def update_stats(self, link) -> None:
+#         if link.is_private:
+#             self.privacy["private"] += 1
+#         if not link.is_private:
+#             self.privacy["public"] += 1
 
+#         for tag in link.get_tags():
+#             self.tags[tag] += 1
 
-class Stats:
-    def __init__(self):
-        self.privacy = defaultdict(int)
-        self.tags = defaultdict(int)
 
-    def update_stats(self, link) -> None:
-        if link.is_private:
-            self.privacy["private"] += 1
-        if not link.is_private:
-            self.privacy["public"] += 1
+# def get_links(filename, private=False):
+#     with open(filename, encoding="utf-8") as f:
+#         soup = BeautifulSoup(f.read(), "html.parser")
+#         for link in soup.find_all("a"):
+#             temp = LinkInfo({**link.attrs, **{"text": link.text}})
+#             if private:
+#                 yield temp
+#             else:
+#                 if not temp.is_private:
+#                     yield temp
 
-        for tag in link.get_tags():
-            self.tags[tag] += 1
 
+# def export_to_csv(filename, links):
+#     with open(filename, encoding="utf-8", mode="w") as f:
+#         writer = csv.writer(f, dialect="unix")
+#         writer.writerow(("Timestamp", "Href"))
 
-def get_links(filename, private=False):
-    with open(filename, encoding="utf-8") as f:
-        soup = BeautifulSoup(f.read(), "html.parser")
-        for link in soup.find_all("a"):
-            temp = LinkInfo({**link.attrs, **{"text": link.text}})
-            if private:
-                yield temp
-            else:
-                if not temp.is_private:
-                    yield temp
+#         writer.writerows(((link.timestamp, link.href) for link in links))
+#         f.flush()
 
 
-def export_to_csv(filename, links):
-    with open(filename, encoding="utf-8", mode="w") as f:
-        writer = csv.writer(f, dialect="unix")
-        writer.writerow(("Timestamp", "Href"))
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description="Analyze your Delicious bookmarks.")
+#     parser.add_argument("-f", action="store", dest="filename", help="Path to file with bookmarks.")
+#     parser.add_argument("-o", action="store", dest="output_file", help="Path to output file", default="report.html")
+#     parser.add_argument(
+#         "--private", action="store_true", dest="process_private", default=False, help="Process private links"
+#     )
+#     parser.add_argument("-csv", action="store", dest="csv_file", help="Path to CSV file for export", default=None)
+#     results = parser.parse_args()
+#     logger.info("Starting with %s" % results)
+#     template = env.get_template("template.html")
 
-        writer.writerows(((link.timestamp, link.href) for link in links))
-        f.flush()
+#     try:
+#         input_file = pathlib.Path(results.filename)
 
+#         with open(results.output_file, "wt") as f:
+#             f.write(template.render(links=(link for link in get_links(input_file, results.process_private))))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Analyze your Delicious bookmarks.")
-    parser.add_argument("-f", action="store", dest="filename", help="Path to file with bookmarks.")
-    parser.add_argument("-o", action="store", dest="output_file", help="Path to output file", default="report.html")
-    parser.add_argument(
-        "--private", action="store_true", dest="process_private", default=False, help="Process private links"
-    )
-    parser.add_argument("-csv", action="store", dest="csv_file", help="Path to CSV file for export", default=None)
-    results = parser.parse_args()
-    logger.info("Starting with %s" % results)
-    template = env.get_template("template.html")
+#         if results.csv_file:
+#             export_to_csv(results.csv_file, get_links(input_file, results.process_private))
 
-    try:
-        input_file = pathlib.Path(results.filename)
+#     except TypeError:
+#         logger.exception("No file is provided", exc_info=True)
+#     except FileNotFoundError:
+#         logger.exception("No such file", exc_info=True)
+#     except PermissionError:
+#         logger.exception("Permission problem", exc_info=True)
+#     finally:
+#         exit()
 
-        with open(results.output_file, "wt") as f:
-            f.write(template.render(links=(link for link in get_links(input_file, results.process_private))))
+#     # db_filename = "links_storage.db"
+#     # schema_filename = "links_schema.sql"
 
-        if results.csv_file:
-            export_to_csv(results.csv_file, get_links(input_file, results.process_private))
+#     # db_is_new = not os.path.exists(db_filename)
 
-    except TypeError:
-        logger.exception("No file is provided", exc_info=True)
-    except FileNotFoundError:
-        logger.exception("No such file", exc_info=True)
-    except PermissionError:
-        logger.exception("Permission problem", exc_info=True)
-    finally:
-        exit()
+#     # with sqlite3.connect(db_filename) as conn:
+#     #     if db_is_new:
+#     #         with open(schema_filename) as f:
+#     #             conn.executescript(f.read())
 
-    # db_filename = "links_storage.db"
-    # schema_filename = "links_schema.sql"
+#     #         cursor = conn.cursor()
 
-    # db_is_new = not os.path.exists(db_filename)
-
-    # with sqlite3.connect(db_filename) as conn:
-    #     if db_is_new:
-    #         with open(schema_filename) as f:
-    #             conn.executescript(f.read())
-
-    #         cursor = conn.cursor()
-
-    #         for link in get_links(results.filename, results.process_private):
-    #             cursor.execute(
-    #                 "INSERT INTO links VALUES (?, ?, ?, ?, ?)",
-    #                 (None, str(link.url), int(link.is_private), link.timestamp, 0),
-    #             )
+#     #         for link in get_links(results.filename, results.process_private):
+#     #             cursor.execute(
+#     #                 "INSERT INTO links VALUES (?, ?, ?, ?, ?)",
+#     #                 (None, str(link.url), int(link.is_private), link.timestamp, 0),
+#     #             )
--- tests/test_analyze_delicious.py
+++ tests/test_analyze_delicious.py
@@ -1,144 +1,162 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-import copy
-from os.path import join, exists
-import csv
-import logging
+# import copy
+# from os.path import join, exists
+# import csv
+# import logging
 
-import pytest
+# import pytest
 
-from analyze_delicious import LinkInfo, Stats, Url, get_links, export_to_csv
+# from analyze_delicious import LinkInfo, Stats, Url, get_links, export_to_csv
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-temp_url = "https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep"
-temp_link = {"href": "https://www.smartvideos.ru/", "add_date": "2012", "private": "1", "tags": "education,imported"}
+# temp_url = "https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep"
+# temp_link = {"href": "https://www.smartvideos.ru/", "add_date": "2012", "private": "1", "tags": "education,imported"}
 
-temp_link_public = copy.deepcopy(temp_link)
-temp_link_public["private"] = "0"
+# temp_link_public = copy.deepcopy(temp_link)
+# temp_link_public["private"] = "0"
 
-temp_link_long_date = copy.deepcopy(temp_link)
-temp_link_long_date["add_date"] = "1487678923"
+# temp_link_long_date = copy.deepcopy(temp_link)
+# temp_link_long_date["add_date"] = "1487678923"
 
-temp_link_empty_tags = copy.deepcopy(temp_link)
-temp_link_empty_tags["tags"] = ""
+# temp_link_empty_tags = copy.deepcopy(temp_link)
+# temp_link_empty_tags["tags"] = ""
 
-delicious_links = join("datasets", "delicious_links_private.html")
+# delicious_links = join("datasets", "delicious_links_private.html")
 
-
-@pytest.mark.console
-class TestLink:
-    def test_url_creation(self):
-        assert Url(temp_url).addr == temp_url
-
-    def test_get_domain_for_url(self):
-        assert Url(temp_url).get_domain() == "plumbr.eu"
-
-    def test_print_url(self):
-        assert Url(temp_url).__str__() == temp_url
+# pytestmark = [pytest.mark.skip]
 
 
-@pytest.mark.console
-class TestLinkInfo:
-    def test_link_info_get_tags(self):
-        assert LinkInfo(temp_link).get_tags() == ["education", "imported"]
+# @pytest.mark.console
+# class TestLink:
+#     def test_url_creation(self):
+#         assert Url(temp_url).addr == temp_url
 
-    def test_is_private(self):
-        assert LinkInfo(temp_link).is_private is True
-        assert LinkInfo(temp_link_public).is_private is False
+#     def test_get_domain_for_url(self):
+#         assert Url(temp_url).get_domain() == "plumbr.eu"
 
-    def test_href(self):
-        assert LinkInfo(temp_link).href == "https://www.smartvideos.ru/"
-
-    def test_text(self):
-        assert LinkInfo({**temp_link, **{"text": "text2"}}).text == "text2"
-        assert LinkInfo({**temp_link, **{"text": ""},}).text == "www.smartvideos.ru"
-        assert LinkInfo({**temp_link, **{"text": "None"},}).text == "www.smartvideos.ru"
-
-    def test_link_date_short(self):
-        assert LinkInfo(temp_link).date == "2012"
-
-    def test_link_date_long(self):
-        assert LinkInfo(temp_link_long_date).date == "2017-02-21"
-
-    def test_link_timestamp_short(self):
-        assert LinkInfo(temp_link).timestamp == 0
-
-    def test_link_timestamp_long(self):
-        assert LinkInfo(temp_link_long_date).timestamp == 1487678923
-
-    def test_link_str_public(self):
-        assert LinkInfo(temp_link_public).__str__() == "LinkInfo: www.smartvideos.ru"
-
-    def test_link_str_private(self):
-        assert LinkInfo(temp_link).__str__() == "LinkInfo is private."
-
-    def test_link_get_domaint(self):
-        assert LinkInfo(temp_link).get_domain() == "www.smartvideos.ru"
+#     def test_print_url(self):
+#         assert Url(temp_url).__str__() == temp_url
 
 
-@pytest.mark.console
-class TestStats:
-    def test_stats_privacy(self):
-        s = Stats()
-        assert s.privacy["private"] == 0
-        assert s.privacy["public"] == 0
-        s.update_stats(LinkInfo(temp_link))
-        assert s.privacy["private"] == 1
-        s.update_stats(LinkInfo(temp_link_public))
-        assert s.privacy["private"] == 1
-        assert s.privacy["public"] == 1
+# @pytest.mark.console
+# class TestLinkInfo:
+#     def test_link_info_get_tags(self):
+#         assert LinkInfo(temp_link).get_tags() == ["education", "imported"]
 
-    def test_stats_tags(self):
-        s = Stats()
-        assert len(s.tags.keys()) == 0
-        s.update_stats(LinkInfo(temp_link))
-        assert len(s.tags.keys()) == 2
-        assert s.tags["imported"] == 1
-        assert s.tags["education"] == 1
+#     def test_is_private(self):
+#         assert LinkInfo(temp_link).is_private is True
+#         assert LinkInfo(temp_link_public).is_private is False
 
-        s.update_stats(LinkInfo(temp_link_empty_tags))
+#     def test_href(self):
+#         assert LinkInfo(temp_link).href == "https://www.smartvideos.ru/"
 
-        assert s.tags["imported"] == 1
-        assert s.tags["education"] == 1
+#     def test_text(self):
+#         assert LinkInfo({**temp_link, **{"text": "text2"}}).text == "text2"
+#         assert (
+#             LinkInfo(
+#                 {
+#                     **temp_link,
+#                     **{"text": ""},
+#                 }
+#             ).text
+#             == "www.smartvideos.ru"
+#         )
+#         assert (
+#             LinkInfo(
+#                 {
+#                     **temp_link,
+#                     **{"text": "None"},
+#                 }
+#             ).text
+#             == "www.smartvideos.ru"
+#         )
+
+#     def test_link_date_short(self):
+#         assert LinkInfo(temp_link).date == "2012"
+
+#     def test_link_date_long(self):
+#         assert LinkInfo(temp_link_long_date).date == "2017-02-21"
+
+#     def test_link_timestamp_short(self):
+#         assert LinkInfo(temp_link).timestamp == 0
+
+#     def test_link_timestamp_long(self):
+#         assert LinkInfo(temp_link_long_date).timestamp == 1487678923
+
+#     def test_link_str_public(self):
+#         assert LinkInfo(temp_link_public).__str__() == "LinkInfo: www.smartvideos.ru"
+
+#     def test_link_str_private(self):
+#         assert LinkInfo(temp_link).__str__() == "LinkInfo is private."
+
+#     def test_link_get_domaint(self):
+#         assert LinkInfo(temp_link).get_domain() == "www.smartvideos.ru"
 
 
-@pytest.mark.console
-class TestGetLInks:
-    def test_private(self):
-        f = get_links(delicious_links, private=True)
-        assert not next(f).is_private is True
-        assert next(f).is_private
-        assert next(f).is_private
-        with pytest.raises(StopIteration):
-            next(f)
+# @pytest.mark.console
+# class TestStats:
+#     def test_stats_privacy(self):
+#         s = Stats()
+#         assert s.privacy["private"] == 0
+#         assert s.privacy["public"] == 0
+#         s.update_stats(LinkInfo(temp_link))
+#         assert s.privacy["private"] == 1
+#         s.update_stats(LinkInfo(temp_link_public))
+#         assert s.privacy["private"] == 1
+#         assert s.privacy["public"] == 1
 
-    def test_public(self):
-        f = get_links(delicious_links)
-        assert not next(f).is_private is True
-        with pytest.raises(StopIteration):
-            next(f)
+#     def test_stats_tags(self):
+#         s = Stats()
+#         assert len(s.tags.keys()) == 0
+#         s.update_stats(LinkInfo(temp_link))
+#         assert len(s.tags.keys()) == 2
+#         assert s.tags["imported"] == 1
+#         assert s.tags["education"] == 1
+
+#         s.update_stats(LinkInfo(temp_link_empty_tags))
+
+#         assert s.tags["imported"] == 1
+#         assert s.tags["education"] == 1
 
 
-@pytest.mark.console
-class TestCsvExport:
-    def test_export_to_csv(self, tmp_path, caplog):
-        caplog.set_level(logging.INFO)
-        export_to_csv(join(tmp_path, "export.csv"), get_links(delicious_links))
-        assert exists(join(tmp_path, "export.csv")) is True
+# @pytest.mark.console
+# class TestGetLInks:
+#     def test_private(self):
+#         f = get_links(delicious_links, private=True)
+#         assert not next(f).is_private is True
+#         assert next(f).is_private
+#         assert next(f).is_private
+#         with pytest.raises(StopIteration):
+#             next(f)
 
-        with open(join(tmp_path, "export.csv")) as f:
-            reader = csv.reader(f)
+#     def test_public(self):
+#         f = get_links(delicious_links)
+#         assert not next(f).is_private is True
+#         with pytest.raises(StopIteration):
+#             next(f)
 
-            titles = next(reader)
-            logger.info(f"Titles: {titles}")
-            assert "Href" in titles
-            assert "Timestamp" in titles
 
-            data = next(reader)
-            logger.info(f"Data: {data}")
-            assert data[0] == "1496225431", f"Data line: {data}"
-            assert (
-                data[1]
-                == "https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep"
-            )
+# @pytest.mark.console
+# class TestCsvExport:
+#     def test_export_to_csv(self, tmp_path, caplog):
+#         caplog.set_level(logging.INFO)
+#         export_to_csv(join(tmp_path, "export.csv"), get_links(delicious_links))
+#         assert exists(join(tmp_path, "export.csv")) is True
+
+#         with open(join(tmp_path, "export.csv")) as f:
+#             reader = csv.reader(f)
+
+#             titles = next(reader)
+#             logger.info(f"Titles: {titles}")
+#             assert "Href" in titles
+#             assert "Timestamp" in titles
+
+#             data = next(reader)
+#             logger.info(f"Data: {data}")
+#             assert data[0] == "1496225431", f"Data line: {data}"
+#             assert (
+#                 data[1]
+#                 == "https://plumbr.eu/handbook/garbage-collection-algorithms-implementations/concurrent-mark-and-sweep"
+#             )
