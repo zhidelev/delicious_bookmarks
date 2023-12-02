527af289e2f368865b3c7e86dd0d367f6fa64cb3
andreiz <mcjanz@gmail.com>
2023-12-02 23:08:06 +0100

    fix: mark tests to xfail

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -1,16 +1,16 @@
 import schemathesis
 import pytest
-from hypothesis import settings, Phase
+from hypothesis import settings
 from schemathesis import DataGenerationMethod
 
-# schema = schemathesis.from_uri(
-#     "http://127.0.0.1:8000/openapi.json",
-#     data_generation_methods=[DataGenerationMethod.positive, DataGenerationMethod.negative],
-# )
+schema = schemathesis.from_uri(
+    "http://127.0.0.1:8000/openapi.json",
+    data_generation_methods=[DataGenerationMethod.positive, DataGenerationMethod.negative],
+)
 
 
-@pytest.mark.skip("")
-# @schema.parametrize()
+@pytest.mark.xfail(reason="experimental")
+@schema.parametrize()
 @settings(max_examples=1000)
 def test_api(case):
     case.call_and_validate()
