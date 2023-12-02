777f42881fc925e1d048f7a59a1f7907cc5d4dce
andreiz <mcjanz@gmail.com>
2023-12-02 23:12:25 +0100

    fix: skip tests

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -3,14 +3,14 @@ import pytest
 from hypothesis import settings
 from schemathesis import DataGenerationMethod
 
-schema = schemathesis.from_uri(
-    "http://127.0.0.1:8000/openapi.json",
-    data_generation_methods=[DataGenerationMethod.positive, DataGenerationMethod.negative],
-)
+# schema = schemathesis.from_uri(
+#     "http://127.0.0.1:8000/openapi.json",
+#     data_generation_methods=[DataGenerationMethod.positive, DataGenerationMethod.negative],
+# )
 
 
+# @schema.parametrize()
 @pytest.mark.skip(reason="experimental")
-@schema.parametrize()
 @settings(max_examples=1000)
 def test_api(case):
     case.call_and_validate()
