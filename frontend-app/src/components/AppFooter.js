423331af538a9e2565be0dd07d3bf9acddfb7dbd
andreiz <mcjanz@gmail.com>
2024-02-07 23:47:56 +0100

    feat: frontend-app with vite

--- frontend-app/src/components/AppFooter.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class AppFooter extends Component {
-    render() {
-        return (
-            <h2>Footer</h2>
-        )
-    }
-}
--- frontend-app/src/components/AppHeader.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AppHeader from './AppHeader';
-
-test('renders App Header', () => {
-  render(<AppHeader />);
-  const header = screen.getByText(/Bookmarks/i);
-  expect(header).toBeInTheDocument();
-});
