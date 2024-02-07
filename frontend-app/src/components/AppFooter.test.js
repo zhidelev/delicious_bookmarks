e42a9261e40cbebc480bacd61da2abcd8b8bc8bc
andreiz <mcjanz@gmail.com>
2024-02-07 23:47:56 +0100

    feat: frontend-app with vite

--- frontend-app/src/components/AppFooter.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AppFooter from './AppFooter';
-
-test('renders App Footer', () => {
-  render(<AppFooter />);
-  const footer = screen.getByText(/Footer/i);
-  expect(footer).toBeInTheDocument();
-});
--- frontend-app/src/components/Bookmark.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Bookmark from './Bookmark.js';
-
-test('renders learn react link', () => {
-  render(<Bookmark />);
-  const linkElement = screen.getByText(/Title/i);
-  expect(linkElement).toBeInTheDocument();
-});
