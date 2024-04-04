99d08e2d76ef00bfd6984784a6e62b023d02e201
andreiz <mcjanz@gmail.com>
2024-04-04 17:56:54 +0200

    fix: update sorting

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,7 +1,7 @@
 from typing import Annotated, List, Union
-from sqlalchemy.orm import Session
 
-from fastapi import FastAPI, Path, Depends
+from fastapi import Depends, FastAPI, Path
+from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -10,7 +10,6 @@ models.Base.metadata.create_all(bind=engine)
 
 
 from .tag import Tags
-
 
 app = FastAPI()
 
