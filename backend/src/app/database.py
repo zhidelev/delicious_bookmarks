ca5700d1a1a53f0e6dcd46fe91ef27d98058ad01
andreiz <mcjanz@gmail.com>
2024-04-04 21:22:51 +0200

    fix: update

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 try:
     from dotenv import load_dotenv
@@ -9,10 +13,6 @@ try:
     load_dotenv()
 except ImportError:
     pass
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres@127.0.0.1/db"
 
 
 SQLALCHEMY_SCHEMA = os.getenv("SQLALCHEMY_SCHEMA")
@@ -23,6 +23,8 @@ DB_NAME = os.getenv("DB_NAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 SQLALCHEMY_DATABASE_URL = f"{SQLALCHEMY_SCHEMA}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+logger.debug(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
