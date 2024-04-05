7c86d2b9db84acf25fef78ec0be8b2b4266e0f83
andreiz <mcjanz@gmail.com>
2024-04-05 14:32:37 +0200

    fix: update ci

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -11,12 +11,12 @@ except ImportError:
     pass
 
 
-SQLALCHEMY_SCHEMA = os.getenv("SQLALCHEMY_SCHEMA")
-DB_USER = os.getenv("DB_USER")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+SQLALCHEMY_SCHEMA = os.getenv("SQLALCHEMY_SCHEMA", "postgresql+pg8000")
+DB_USER = os.getenv("DB_USER", "user")
+DB_HOST = os.getenv("DB_HOST", "hot")
+DB_PORT = os.getenv("DB_PORT", 5432)
+DB_NAME = os.getenv("DB_NAME", "name")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 
 SQLALCHEMY_DATABASE_URL = f"{SQLALCHEMY_SCHEMA}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
