381b4394d56a44719c44456409a07d7785bdac7c
andreiz <mcjanz@gmail.com>
2024-04-15 19:53:43 +0200

    fix: add pools for db

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -14,7 +14,7 @@ except ImportError:
 
 SQLALCHEMY_SCHEMA = os.getenv("SQLALCHEMY_SCHEMA", "postgresql+pg8000")
 DB_USER = os.getenv("DB_USER", "user")
-DB_HOST = os.getenv("DB_HOST", "hot")
+DB_HOST = os.getenv("DB_HOST", "host")
 DB_PORT = os.getenv("DB_PORT", 5432)
 DB_NAME = os.getenv("DB_NAME", "name")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
@@ -22,7 +22,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 SQLALCHEMY_DATABASE_URL = f"{SQLALCHEMY_SCHEMA}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
